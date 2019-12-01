e170126da2ff58b63e652d6c3b88bb3b6b414b5b
Tony Zhang <tony.zhang.domore@outlook.com>
2019-12-01 13:25:46 -0800

    Initial Commit

---
+++ Camera Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Camera Test
+//
+//  Created by Tony Zhang on 2019-12-01.
+//  Copyright © 2019 Tony Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Camera TestTests/Camera_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  Camera_TestTests.swift
+//  Camera TestTests
+//
+//  Created by Tony Zhang on 2019-12-01.
+//  Copyright © 2019 Tony Zhang. All rights reserved.
+//
+
+import XCTest
+@testable import Camera_Test
+
+class Camera_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
